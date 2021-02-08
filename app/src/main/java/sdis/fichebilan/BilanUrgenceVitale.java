2a13b70b1d8c8af9e1eddb39bbde838328fb23d4
Raphaël P <56355073+R-17@users.noreply.github.com>
2021-02-08 10:12:27 +0100

    interface

---
+++ app/src/main/java/sdis/fichebilan/BilanUrgenceVitale.java
@@ -0,0 +1,13 @@
+package sdis.fichebilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BilanUrgenceVitale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bilan_urgence_vitale_activity);
+    }
+}
---
+++ app/src/main/java/sdis/fichebilan/DetresseVitaleNeurologiqueActivity.java
@@ -0,0 +1,13 @@
+package sdis.fichebilan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetresseVitaleNeurologiqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detresse_vitale_neurologique_activity);
+    }
+}
