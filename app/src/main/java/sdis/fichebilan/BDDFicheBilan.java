0ddf49bebc7623173bd953988772730843f8cf7e
hugoboss3737 <56357921+hugoboss3737@users.noreply.github.com>
2021-02-11 17:11:32 +0100

    creation de la BDD (pas terminer)

--- app/src/main/java/sdis/fichebilan/BDDFicheBilan.java
+++ app/src/main/java/sdis/fichebilan/BDDFicheBilan.java
@@ -4,10 +4,11 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public abstract class BDDFicheBilan extends SQLiteOpenHelper {
+public class BDDFicheBilan extends SQLiteOpenHelper {
 
     private static final int VERSION_BDD = 1;
     private static final String NOM_BDD="BDDFicheBilan";
+
 
     public BDDFicheBilan(Context context){
         super(context, NOM_BDD, null, VERSION_BDD);
@@ -15,41 +16,59 @@ public abstract class BDDFicheBilan extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase objbdd){
-        String Bilan = "CREATE TABLE Bilan(id INT,dateInitiale TEXT,heureInitiale TEXT,num INT NOT NULL,id_1 INT NOT NULL,PRIMARY KEY(id),FOREIGN KEY(num) REFERENCES Intervention(num),FOREIGN KEY(id_1) REFERENCES ChefAgres(id));";
-        String ChefAgres = "CREATE TABLE ChefAgres(id INT,nom TEXT,prenom TEXT,signature BLOB,id_1 INT NOT NULL,PRIMARY KEY(id),FOREIGN KEY(id_1) REFERENCES CentreSecours(id));";
-        String CentreSecours = "CREATE TABLE CentreSecours (id INTEGER PRIMARY KEY,nom TEXT);";
-        String Victime = "CREATE TABLE Victime(id INT,nom TEXT,prenom TEXT,DDN TEXT,sexe INT,adresse TEXT,CP TEXT,ville TEXT,id_1 INT NOT NULL,PRIMARY KEY(id),FOREIGN KEY(id_1) REFERENCES Bilan(id));";
-        String Intervention = "CREATE TABLE Intervention(num INT,adresse TEXT,heurePresumee TEXT,lieu TEXT,details TEXT,PRIMARY KEY(num));";
-        String Malaise = "CREATE TABLE Malaise(num INT,PRIMARY KEY(num),FOREIGN KEY(num) REFERENCES Intervention(num));";
-        String Autre = "CREATE TABLE Autre(num INT,PRIMARY KEY(num),FOREIGN KEY(num) REFERENCES Intervention(num));";
-        String Accident = "CREATE TABLE Accident(num INT,PRIMARY KEY(num),FOREIGN KEY(num) REFERENCES Intervention(num));";
-        String Accident_divers = "CREATE TABLE Accident_divers(num INT,PRIMARY KEY(num),FOREIGN KEY(num) REFERENCES Accident(num));";
-        String Accident_de_la_route = "CREATE TABLE Accident_de_la_route(num INT,blesse TEXT,contre TEXT,PRIMARY KEY(num),FOREIGN KEY(num) REFERENCES Accident(num));";
-        String Surveillance = "CREATE TABLE Surveillance(id INT,heure TEXT,infoComplementaire TEXT,evacuationVers TEXT,evacuationPar TEXT,evacuationMedicalisee INT,evacuationMedicaliseeAvec TEXT,evacuationIntubation INT,evacuationPerfusion INT,evacuationScoreGlasgow INT,PRIMARY KEY(id),FOREIGN KEY(id) REFERENCES Bilan(id));";
-        String DetresseVitale = "CREATE TABLE DetresseVitale(id INT,conscienceParole INT,conscienceDesoriente INT,conscienceSomnolence INT,conscienceAgitation INT,consciencePci INT,consciencePciDuree INT,sensibiliteSuperieure INT,sensibiliteInferieure INT,reactionYeuxSeul INT,reactionYeuxOrdre INT,reactionPupile INT,reactionJambeSeul INT,reactionJambeOrdre INT,reactionBrasSeul INT,reactionBrasOrdre INT,cardioPoulsRadial INT,cardioPoulsRadialRegulier INT,cardioPoulsFrequence INT,cardioPaleur INT,cardioAutoTensionSystolique INT,cardioAutoTensionDiastolique INT,ventilationFrequence INT,ventilationFrequenceReguliere INT,ventilationSensationEtouffement INT,ventilationSifflementRaleGargouillement INT,ventalationCyanose INT,ventilationSueur INT,ventilationSaturation INT,ventilationSaturationTypeAir TEXT,PRIMARY KEY(id),FOREIGN KEY(id) REFERENCES Bilan(id));";
-        String UrgenceVitale = "CREATE TABLE UrgenceVitale(id INT,hemoragie INT,conscience INT,ventilation INT,poulsCarotidient INT,PRIMARY KEY(id),FOREIGN KEY(id) REFERENCES Bilan(id));";
-        String Complementaire = "CREATE TABLE Complementaire(id INT,hopital TEXT,traitement TEXT,avoirAlergie INT,alergie TEXT NOT NULL,plainte TEXT,evs INT,gesteImmobilisation INT,gesteVentilation INT,gesteRcp INT,gesteDsa INT,gesteRcpDuree INT,gesteOxygene INT,gestedebitOxygene INT,gesteCollierCervical INT,gesteAtelle INT,gesteGelEau INT,gesteDemiAssis INT,gestePlatDos INT,gestePls INT,PRIMARY KEY(id),FOREIGN KEY(id) REFERENCES Bilan(id));";
-        String Traumatisme = "CREATE TABLE Traumatisme(id INT,description TEXT,id_1 INT NOT NULL,id_2 INT NOT NULL,PRIMARY KEY(id),FOREIGN KEY(id_1) REFERENCES TypeTraumatisme(id),FOREIGN KEY(id_2) REFERENCES Complementaire(id));";
-        String TypeTraumatisme = "CREATE TABLE TypeTraumatisme(id INT,libelle VARCHAR(50),PRIMARY KEY(id));";
 
-
-
-        objbdd.execSQL(Bilan);
-        objbdd.execSQL(ChefAgres);
-        objbdd.execSQL(CentreSecours);
-        objbdd.execSQL(Victime);
+        String Intervention = "CREATE TABLE Intervention(num INT PRIMARY KEY,adresse TEXT,heurePresumee TEXT,lieu TEXT,details TEXT);";
         objbdd.execSQL(Intervention);
-        objbdd.execSQL(Malaise);
-        objbdd.execSQL(Autre);
+
+        String Accident = "CREATE TABLE Accident(num INT PRIMARY KEY,FOREIGN KEY(num) REFERENCES Intervention(num));";
         objbdd.execSQL(Accident);
+
+        String Malaise = "CREATE TABLE Malaise(num INT PRIMARY KEY,FOREIGN KEY(num) REFERENCES Intervention(num));";
+        objbdd.execSQL(Malaise);
+
+        String Autre = "CREATE TABLE Autre(num INT PRIMARY KEY,FOREIGN KEY(num) REFERENCES Intervention(num));";
+        objbdd.execSQL(Autre);
+
+        String Accident_divers = "CREATE TABLE Accident_divers(num INT PRIMARY KEY,FOREIGN KEY(num) REFERENCES Accident(num));";
         objbdd.execSQL(Accident_divers);
-        objbdd.execSQL(Accident_de_la_route);
-        objbdd.execSQL(Surveillance);
-        objbdd.execSQL(DetresseVitale);
-        objbdd.execSQL(UrgenceVitale);
-        objbdd.execSQL(Complementaire);
-        objbdd.execSQL(Traumatisme);
+
+        String CentreSecours = "CREATE TABLE CentreSecours (id INTEGER PRIMARY KEY,nom TEXT);";
+        objbdd.execSQL(CentreSecours);
+
+        String TypeTraumatisme = "CREATE TABLE TypeTraumatisme(id INT,libelle TEXT,PRIMARY KEY(id));";
         objbdd.execSQL(TypeTraumatisme);
+
+        String Accident_de_la_route = "CREATE TABLE Accident_de_la_route(num INT,blesse TEXT,contre TEXT,FOREIGN KEY(num) REFERENCES Accident(num));";
+        objbdd.execSQL(Accident_de_la_route);
+
+        String ChefAgres = "CREATE TABLE ChefAgres(id INT PRIMARY KEY,nom TEXT,prenom TEXT,signature BLOB,id_1 INT NOT NULL,FOREIGN KEY(id_1) REFERENCES CentreSecours(id));";
+        objbdd.execSQL(ChefAgres);
+
+        String Bilan = "CREATE TABLE Bilan(id INT PRIMARY KEY,dateInitiale TEXT,heureInitiale TEXT,num INT NOT NULL,id_1 INT NOT NULL,FOREIGN KEY(num) REFERENCES Intervention(num),FOREIGN KEY(id_1) REFERENCES ChefAgres(id));";
+        objbdd.execSQL(Bilan);
+
+        String DetresseVitale = "CREATE TABLE DetresseVitale(id INT PRIMARY KEY,conscienceParole INT,conscienceDesoriente INT,conscienceSomnolence INT,conscienceAgitation INT,consciencePci INT,consciencePciDuree INT,sensibiliteSuperieure INT,sensibiliteInferieure INT,reactionYeuxSeul INT,reactionYeuxOrdre INT,reactionPupile INT,reactionJambeSeul INT,reactionJambeOrdre INT,reactionBrasSeul INT,reactionBrasOrdre INT,cardioPoulsRadial INT,cardioPoulsRadialRegulier INT,cardioPoulsFrequence INT,cardioPaleur INT,cardioAutoTensionSystolique INT,cardioAutoTensionDiastolique INT,ventilationFrequence INT,ventilationFrequenceReguliere INT,ventilationSensationEtouffement INT,ventilationSifflementRaleGargouillement INT,ventalationCyanose INT,ventilationSueur INT,ventilationSaturation INT,ventilationSaturationTypeAir TEXT,FOREIGN KEY(id) REFERENCES Bilan(id));";
+        objbdd.execSQL(DetresseVitale);
+
+        String UrgenceVitale = "CREATE TABLE UrgenceVitale(id INT PRIMARY KEY,hemoragie INT,conscience INT,ventilation INT,poulsCarotidient INT,FOREIGN KEY(id) REFERENCES Bilan(id));";
+        objbdd.execSQL(UrgenceVitale);
+
+        String Surveillance = "CREATE TABLE Surveillance(id INT PRIMARY KEY,heure TEXT,infoComplementaire TEXT,evacuationVers TEXT,evacuationPar TEXT,evacuationMedicalisee INT,evacuationMedicaliseeAvec TEXT,evacuationIntubation INT,evacuationPerfusion INT,evacuationScoreGlasgow INT,PRIMARY KEY(id),FOREIGN KEY(id) REFERENCES Bilan(id));";
+        objbdd.execSQL(Surveillance);
+
+        String Complementaire = "CREATE TABLE Complementaire(id INT PRIMARY KEY,hopital TEXT,traitement TEXT,avoirAlergie INT,alergie TEXT NOT NULL,plainte TEXT,evs INT,gesteImmobilisation INT,gesteVentilation INT,gesteRcp INT,gesteDsa INT,gesteRcpDuree INT,gesteOxygene INT,gestedebitOxygene INT,gesteCollierCervical INT,gesteAtelle INT,gesteGelEau INT,gesteDemiAssis INT,gestePlatDos INT,gestePls INT,FOREIGN KEY(id) REFERENCES Bilan(id));";
+        objbdd.execSQL(Complementaire);
+
+        String Traumatisme = "CREATE TABLE Traumatisme(id INT PRIMARY KEY,description TEXT,id_1 INT NOT NULL,id_2 INT NOT NULL,FOREIGN KEY(id_1) REFERENCES TypeTraumatisme(id),FOREIGN KEY(id_2) REFERENCES Complementaire(id));";
+        objbdd.execSQL(Traumatisme);
+
+        String Victime = "CREATE TABLE Victime(id INT PRIMARY KEY,nom TEXT,prenom TEXT,DDN TEXT,sexe INT,adresse TEXT,CP TEXT,ville TEXT,id_1 INT NOT NULL,FOREIGN KEY(id_1) REFERENCES Bilan(id));";
+        objbdd.execSQL(Victime);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 
--- app/src/main/java/sdis/fichebilan/MainActivity.java
+++ app/src/main/java/sdis/fichebilan/MainActivity.java
@@ -1,13 +1,24 @@
 package sdis.fichebilan;
 
+import android.database.sqlite.SQLiteDatabase;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // création instanciation de l'outil SQLiteOpenHelper
+        BDDFicheBilan maPremBase = new BDDFicheBilan(this);
+        //création de ma BddSQLiteDatabase
+        SQLiteDatabase MaFicheBilan = maPremBase.getWritableDatabase();
+
+        //MaFicheBilan.close();
+        //maPremBase.close();
     }
+
 }
